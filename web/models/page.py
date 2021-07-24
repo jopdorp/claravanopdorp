585a6bd949de3049078766a66fc138cada8f8bc3
Jegor van Opdorp <jegor@leaf.cloud>
2021-07-24 14:37:42 +0200

    added blank=True to page.content

--- web/models/page.py
+++ web/models/page.py
@@ -5,7 +5,7 @@ class Page(models.Model):
 
     title = models.CharField(max_length=20, default="")
     path = models.CharField(max_length=20, default="")
-    content = models.TextField()
+    content = models.TextField(blank=True)
     is_in_menu = models.BooleanField()
 
     def __str__(self):
