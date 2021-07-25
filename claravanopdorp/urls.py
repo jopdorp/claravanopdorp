9b2ea184b3bbe04796138335912684d5cf9e8353
Clara van Opdorp <claravanopdorp@gmail.com>
2021-07-25 10:00:10 +0200

    added new logo as favicons, fixed overlapping menu with logo, added urls for new favicons

--- claravanopdorp/urls.py
+++ claravanopdorp/urls.py
@@ -18,15 +18,20 @@ from django.urls import path
 from web.views import index, robots
 from django.views.generic.base import RedirectView
 from django.contrib.staticfiles.storage import staticfiles_storage
+from os import listdir
+from pathlib import Path
 
+def get_favicon_paths():
+    filenames = listdir(Path(__file__).parent.absolute().__str__() + "/../web/static/web/favicons/")
+    return [path(
+        name,
+        RedirectView.as_view(url=staticfiles_storage.url("web/favicons/" + name)),
+    ) for name in filenames]
 
 urlpatterns = [
     path("robots.txt", robots.robots_txt),
     path('admin/', admin.site.urls),
     path('', index.Home.as_view()),
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=staticfiles_storage.url("web/favicon.png")),
-    ),
+] + get_favicon_paths() + [
     path('<page>', index.Home.as_view()),
 ]
