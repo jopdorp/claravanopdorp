341fceb16e81ebbc6149a03c82948c70abf6b756
Jegor van Opdorp <jegorvanopdorp@gmail.com>
2021-11-27 16:06:18 +0000

    updated contact and main

--- claravanopdorp/settings.py
+++ claravanopdorp/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'claravanopdorp.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'claravanopdorp',
+        'USER': 'postgres',
+        'PASSWORD': 'D5WyqUHH4MsxWvq4',
+        'HOST': 'localhost',
     }
+
 }
 
 # Password validation
@@ -138,4 +143,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'claravanopdorp.projects@gmail.com'
-
+EMAIL_HOST_PASSWORD = 'silenciobrunoCMJ'
