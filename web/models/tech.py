3652b6e014528c51cc477435f6df902eb8ca69a4
Clara van Opdorp <claravanopdorp@gmail.com>
2022-04-12 15:35:04 +0200

    create icons and style it in case page, and added git hub icons

--- web/models/tech.py
+++ web/models/tech.py
@@ -5,7 +5,8 @@ from web.models.work import Work
 class TechnologyIcons(models.Model):
 
     title = models.CharField(max_length=20, default="", blank=True)
-    content = models.TextField()
+    icon_class = models.CharField(max_length=64, default="", blank=True)
+    url = models.CharField(max_length=256, default="", blank=True)
     works = models.ManyToManyField(Work)
 
     def __str__(self):
--- web/models/work.py
+++ web/models/work.py
@@ -5,11 +5,12 @@ from django_quill.fields import QuillField
 class Work(models.Model):
 
     title = models.CharField(max_length=30, default="")
-    description = models.TextField(blank=True)
+    description = models.CharField(max_length=500, default="",blank=True)
     content = models.TextField(blank=True)
     is_published = models.BooleanField()
     video = models.FileField(default="")
-    url = models.CharField(max_length=30, default="")
+    url = models.CharField(max_length=256, default="")
+    github_url = models.CharField(max_length=256, default="")
 
     def __str__(self):
         return 'Work: ' + self.title
@@ -18,8 +19,9 @@ class WorkSection(models.Model):
 
     title = models.CharField(max_length=20, default="",blank=True)
     reference = models.TextField(blank=True)
-    content = QuillField(blank=True, default=None, )
+    content = QuillField(default="", blank=True, null=True)
     photo = models.ImageField(default="", blank=True, null=True)
+    url = models.CharField(max_length=256, default="", blank=True, null=True)
     feet = models.TextField(blank=True)
     work = models.ForeignKey(Work, on_delete=models.CASCADE)
 
